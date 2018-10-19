bf9f45fe39da3de4c8a09f209dea8c8c30d9da7e
Pramod Guruprasad <pdg.august@gmail.com>
2018-10-19 14:39:17 +0200

    Initial Commit

---
+++ tabletry/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tabletry
+//
+//  Created by Pramod Guruprasad on 19/10/18.
+//  Copyright © 2018 Pramod Guruprasad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
